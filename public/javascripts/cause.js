87a7f17865b2f24786a994bdfb3ce63e8ff73124
Jeet Shah <jeetshah4321@gmail.com>
2018-03-04 06:40:06 -0500

    All slide values are percentages, stays below 100%

--- public/javascripts/cause.js
+++ public/javascripts/cause.js
@@ -6,12 +6,14 @@ function init(){
     var veteransNum = document.getElementById('veterans');
     var donateNum = document.getElementById('BackToUs');
 
-    document.getElementById('educationLabel').innerHTML = educationNum.value;
-    document.getElementById('waterLabel').innerHTML = waterNum.value;
-    document.getElementById('disasterLabel').innerHTML = disasterNum.value;
-    document.getElementById('homelessLabel').innerHTML = homelessNum.value;
-    document.getElementById('veteranLabel').innerHTML = veteransNum.value;
-    document.getElementById('donateLabel').innerHTML = donateNum.value;
+    setLabels(educationNum.value, waterNum.value, disasterNum.value, homelessNum.value, veteransNum.value, donateNum.value);
+
+    // document.getElementById('educationLabel').innerHTML = educationNum.value;
+    // document.getElementById('waterLabel').innerHTML = waterNum.value;
+    // document.getElementById('disasterLabel').innerHTML = disasterNum.value;
+    // document.getElementById('homelessLabel').innerHTML = homelessNum.value;
+    // document.getElementById('veteranLabel').innerHTML = veteransNum.value;
+    // document.getElementById('donateLabel').innerHTML = donateNum.value;
 
     educationNum.oninput = function(){
         document.getElementById('educationLabel').innerHTML = educationNum.value;
@@ -40,7 +42,7 @@ function init(){
         // The data for our dataset
         data: {
             datasets: [{
-                data: [educationNum.value, waterNum.value, disasterNum.value, homelessNum.value, veteransNum.value, donateNum.value],
+                data: returnPercentages(educationNum.value, waterNum.value, disasterNum.value, homelessNum.value, veteransNum.value, donateNum.value),
                 backgroundColor: [
                     '#b3ecff',
                     '#e6ccff',
@@ -65,30 +67,56 @@ function init(){
         options: {}
     });
     educationNum.onmouseup = function(){
-        chart.chart.config.data.datasets[0].data = [educationNum.value, waterNum.value, disasterNum.value, homelessNum.value, veteransNum.value, donateNum.value];
+        chart.chart.config.data.datasets[0].data = returnPercentages(educationNum.value, waterNum.value, disasterNum.value, homelessNum.value, veteransNum.value, donateNum.value);
         chart.update();
+        setLabels(educationNum.value, waterNum.value, disasterNum.value, homelessNum.value, veteransNum.value, donateNum.value);
     };
     waterNum.onmouseup = function(){
-        chart.chart.config.data.datasets[0].data = [educationNum.value, waterNum.value, disasterNum.value, homelessNum.value, veteransNum.value, donateNum.value];
+        chart.chart.config.data.datasets[0].data = returnPercentages(educationNum.value, waterNum.value, disasterNum.value, homelessNum.value, veteransNum.value, donateNum.value);
         chart.update();
+        setLabels(educationNum.value, waterNum.value, disasterNum.value, homelessNum.value, veteransNum.value, donateNum.value);
     };
     disasterNum.onmouseup = function(){
-        chart.chart.config.data.datasets[0].data = [educationNum.value, waterNum.value, disasterNum.value, homelessNum.value, veteransNum.value, donateNum.value];
+        chart.chart.config.data.datasets[0].data = returnPercentages(educationNum.value, waterNum.value, disasterNum.value, homelessNum.value, veteransNum.value, donateNum.value);
         chart.update();
+        setLabels(educationNum.value, waterNum.value, disasterNum.value, homelessNum.value, veteransNum.value, donateNum.value);
     };
     homelessNum.onmouseup = function(){
-        chart.chart.config.data.datasets[0].data = [educationNum.value, waterNum.value, disasterNum.value, homelessNum.value, veteransNum.value, donateNum.value];
+        chart.chart.config.data.datasets[0].data = returnPercentages(educationNum.value, waterNum.value, disasterNum.value, homelessNum.value, veteransNum.value, donateNum.value);
         chart.update();
+        setLabels(educationNum.value, waterNum.value, disasterNum.value, homelessNum.value, veteransNum.value, donateNum.value);
     };
     veteransNum.onmouseup = function(){
-        chart.chart.config.data.datasets[0].data = [educationNum.value, waterNum.value, disasterNum.value, homelessNum.value, veteransNum.value, donateNum.value];
+        chart.chart.config.data.datasets[0].data = returnPercentages(educationNum.value, waterNum.value, disasterNum.value, homelessNum.value, veteransNum.value, donateNum.value);
         chart.update();
+        setLabels(educationNum.value, waterNum.value, disasterNum.value, homelessNum.value, veteransNum.value, donateNum.value);
     };
     donateNum.onmouseup = function(){
-        chart.chart.config.data.datasets[0].data = [educationNum.value, waterNum.value, disasterNum.value, homelessNum.value, veteransNum.value, donateNum.value];
+        chart.chart.config.data.datasets[0].data = returnPercentages(educationNum.value, waterNum.value, disasterNum.value, homelessNum.value, veteransNum.value, donateNum.value);
         chart.update();
+        setLabels(educationNum.value, waterNum.value, disasterNum.value, homelessNum.value, veteransNum.value, donateNum.value);
     };
 }
-
+function setLabels(edu, water, dis, home, vet, back){
+    var sum = Number(edu) + Number(water) + Number(dis) + Number(home) + Number(vet) + Number(back);
+    document.getElementById('educationLabel').innerHTML = Math.round((Number(edu)/sum)*100).toString() + '%';
+    document.getElementById('waterLabel').innerHTML = Math.round((Number(water)/sum)*100).toString() + '%';
+    document.getElementById('disasterLabel').innerHTML = Math.round((Number(dis)/sum)*100).toString() + '%';
+    document.getElementById('homelessLabel').innerHTML = Math.round((Number(home)/sum)*100).toString() + '%';
+    document.getElementById('veteranLabel').innerHTML = Math.round((Number(vet)/sum)*100).toString() + '%';
+    document.getElementById('donateLabel').innerHTML = Math.round((Number(back)/sum)*100).toString() + '%';
+}
+function returnPercentages(edu, water, dis, home, vet, back){
+    var sum = Number(edu) + Number(water) + Number(dis) + Number(home) + Number(vet) + Number(back);
+    var numbers = [
+        Math.round((Number(edu)/sum)*100),
+        Math.round((Number(water)/sum)*100),
+        Math.round((Number(dis)/sum)*100),
+        Math.round((Number(home)/sum)*100),
+        Math.round((Number(vet)/sum)*100),
+        Math.round((Number(back)/sum)*100)
+    ];
+    return numbers;
+}
 
 window.onload = init;
