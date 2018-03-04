d9709afbf286f872742f1ca42cbf4aaf1c7c9677
Joseph Krusling <joseph@krusling.net>
2018-03-03 20:53:26 -0500

    added transactions backend, put db in separate file

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var sassMiddleware = require('node-sass-middleware');
 
 var index = require('./routes/index');
 var budget = require('./routes/budget');
+var transactions = require('./routes/transactions');
 
 var app = express();
 
@@ -31,6 +32,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/budget', budget);
+app.use('/transactions', transactions);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
