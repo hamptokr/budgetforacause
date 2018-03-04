73b85852a779956f47d845fdb4d175cea50f69dc
Joseph Krusling <joseph@krusling.net>
2018-03-03 21:31:11 -0500

    cause backend has data now

--- routes/cause.js
+++ routes/cause.js
@@ -1,8 +1,34 @@
 var express = require('express');
+var db = require('../database.js');
 var router = express.Router();
 
 router.get('/', function(req,res,next){
-   res.render('cause');
+   getCauseArray(42, function(cause_array) {
+      console.log(cause_array);
+       res.render('cause', {cause_data: cause_array});
+   });
 });
+
+
+function getCauseArray(user_id, callback) {
+    var connection = db.getDatabaseConnection();
+
+    connection.query('SELECT * FROM cause_split WHERE user_id = ?', [user_id], function (error, results, fields) {
+        if (error) throw error;
+
+        var cause_array = [];
+        for (var i = 0; i < results.length; i++) {
+            var row = results[i];
+            cause_array.push({
+                charityType: row['charity_type'],
+                percentage: row['percentage']
+            });
+            console.log(row);
+        }
+        callback(cause_array);
+    });
+
+    connection.end();
+}
 
 module.exports = router;
