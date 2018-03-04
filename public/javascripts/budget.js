036f80de3a041d06e65d764fbab4a95bf536e4fd
Jeet Shah <jeetshah4321@gmail.com>
2018-03-04 05:52:57 -0500

    Converted budget chart to chart.js

--- public/javascripts/budget.js
+++ public/javascripts/budget.js
@@ -1,28 +1,55 @@
 function init() {
-  let categories = [];
-  let target = [];
-  let actual = [];
-  for (let i = 0; i < data.length; i++) {
+  var categories = [];
+  var target = [];
+  var actual = [];
+  for (var i = 0; i < data.length; i++) {
     categories.push(data[i].category);
     target.push(data[i].target);
     actual.push(data[i].actual);
   }
-  new Chartist.Bar('#budget-chart', {
-    labels: categories,
-    series: [
-      target,
-      actual
-    ]
-  }, {
-    seriesBarDistance: 10,
-    axisX: {
-      offset: 60
-    },
-    axisY: {
-      offset: 80,
-      scaleMinSpace: 15
-    }
+    var barChartData = {
+        labels: categories,
+        datasets: [{
+            label: 'Current',
+            backgroundColor: '#99ffd6',
+            borderColor: '#99ffd6',
+            borderWidth: 1,
+            data: actual
+        }, {
+            label: 'Goal',
+            backgroundColor: '#1ae19e',
+            borderColor: '#1ae19e',
+            borderWidth: 1,
+            data: target
+        }]
+    };
+  var ctx = document.getElementById('canvas').getContext('2d');
+  window.myBar = new Chart(ctx, {
+      type: 'bar',
+      data: barChartData,
+      options: {
+          responsive: true,
+          legend: {
+              position: 'bottom',
+          }
+      }
   });
+  // new Chartist.Bar('#budget-chart', {
+  //   labels: categories,
+  //   series: [
+  //     target,
+  //     actual
+  //   ]
+  // }, {
+  //   seriesBarDistance: 10,
+  //   axisX: {
+  //     offset: 60
+  //   },
+  //   axisY: {
+  //     offset: 80,
+  //     scaleMinSpace: 15
+  //   }
+  // });
 }
 
 window.onload = init;
